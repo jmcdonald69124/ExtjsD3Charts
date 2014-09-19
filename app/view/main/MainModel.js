3024f2e19e21730024986bf9c5168a9ced16531c
Joshua McDonald <joshuamcdonald69124@gmail.com>
2014-09-18 21:48:41 -0400

    Sample Extjs App

---
+++ app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('ExtD3ChartDemo.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'ExtD3ChartDemo'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
